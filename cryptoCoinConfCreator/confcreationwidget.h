dd5b677a0cd968c60547f0354d7bc836f4e3041d
cryptaxe <cryptaxe@plebian>
2014-12-23 14:34:48 -0800

    Added private variables for storing settings

--- cryptoCoinConfCreator/confcreationwidget.h
+++ cryptoCoinConfCreator/confcreationwidget.h
@@ -22,8 +22,62 @@ private slots:
 
     void on_radioButton_linux_toggled(bool checked);
 
+    void on_radioButton_mac_toggled(bool checked);
+
+    void on_radioButton_windows_toggled(bool checked);
+
+    void on_checkBox_server_toggled(bool checked);
+
+    void on_checkBox_generate_toggled(bool checked);
+
+    void on_radioButton_addNodes_toggled(bool checked);
+
+    void on_radioButton_connectOnly_toggled(bool checked);
+
+    void on_checkBox_testnet_toggled(bool checked);
+
+    void on_checkBox_startMini_toggled(bool checked);
+
+    void on_checkBox_startTrayed_toggled(bool checked);
+
+    void on_checkBox_receiveByIP_toggled(bool checked);
+
+    void on_comboBox_selectCoin_currentIndexChanged(int index);
+
+    void on_pushButton_cancel_clicked();
+
+    void on_pushButton_Save_clicked();
+
 private:
     Ui::ConfCreationWidget *ui;
+    int selectCoinIndex;
+    /*
+     * 0 = Linux
+     * 1 = Mac OSX
+     * 2 = Windows
+     */
+    int selectedOS;
+    QString confDirectory;
+    bool serverEnabled;
+    int RPCPort;
+    QString RPCUser;
+    QString RPCPassword;
+    bool generateEnabled;
+    bool addNodesEnabled;
+    bool connectOnlyEnabled;
+    QString node1;
+    QString node2;
+    QString node3;
+    QString node4;
+    QString node5;
+    QString node6;
+    QString node7;
+    bool testnetEnabled;
+    bool startMinEnabled;
+    bool startTrayedEnabled;
+    bool receiveByIPEnabled;
+    int maxConnections;
+    int rpcTimeout;
 };
 
 #endif // CONFCREATIONWIDGET_H
